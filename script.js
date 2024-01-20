9d6625d871dcb8b613b4797795ce319fddeee563
Kevin Wang <kevin.wang7@mail.mcgill.ca>
2024-01-19 21:54:00 -0500

    Clean user interactions
    This includes creating reset function to reset game after done, smoother
    updates of scores, and alert box for winner.

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const HANDS = ['Rock', 'Paper', 'Scissors'];
 let playerScore = 0;
 let computerScore = 0;
+let gameDone = false; 
 
 function getComputerChoice() {
     // get random integer from 0-2
@@ -25,86 +26,119 @@ function playRound(playerSelection, computerSelection) {
     }
 }
 
-function displayPlayerWon() {
-
-}
-
-function displayComputerWon() {
-
-}
-
-function updateScore(result) {
-    let resultArray = result.split(" ");
-    if (resultArray[1] === "win!") {
-        playerScore++;
-
-        // change player score display
-        playerScoreText.textContent = playerScore;
-
-        if (playerScore === 5) {
-            displayPlayerWon();
-        }
-    } else if (resultArray[1] === "lose!") {
-        computerScore++;
-
-        // change computer score display
-        computerScoreText.textContent = computerScore;
-
-        if (computerScore === 5) {
-            displayComputerWon()
-        }
-    }
-}
-
+// interactions
 const playerHand = document.querySelector(".player-hand");
 const computerHand = document.querySelector(".computer-hand");
 const roundResult = document.querySelector(".round-result");
 const playerScoreText = document.querySelector(".player-score .number");
 const computerScoreText = document.querySelector(".computer-score .number")
 
+function displayPlayerWon() {
+    const playAgain = confirm('Congratulations! You won!\nDo you want to play again?');
+    if (playAgain) {
+        reset();
+    } else {
+        gameDone = true;
+    }
+}
+
+function displayComputerWon() {
+    const playAgain = confirm('Oh no! You lost to the computer!\nDo you want to play again?');
+    if (playAgain) {
+        reset();
+    } else {
+        gameDone = true;
+    }
+}
+
+function reset() {
+    playerScore = 0;
+    computerScore = 0;
+    playerScoreText.textContent = playerScore;
+    computerScoreText.textContent = computerScore;
+    computerHand.textContent = '';
+    playerHand.textContent = '';
+    roundResult.textContent = '';
+}
+
+function updateScore(result) {
+    let resultArray = result.split(" ");
+    if (resultArray[1] === "win!") {
+        playerScore++;
+        console.log(playerScore)
+
+        // change player score display
+        playerScoreText.textContent = playerScore;
+       
+        if (playerScore === 5) {
+            setTimeout(function() {
+                displayPlayerWon();
+            }, 0)
+        }
+    } else if (resultArray[1] === "lose!") {
+        computerScore++;
+        console.log(computerScore)
+
+        // change computer score display
+        computerScoreText.textContent = computerScore;
+       
+        if (computerScore === 5) {
+            setTimeout(function() {
+                displayComputerWon();
+            }, 0)
+        }
+    }
+}
+
 // rock
 const rockSection = document.querySelector(".rock")
 rockSection.addEventListener("click", function () {
-    const computerSelection = getComputerChoice();
-    const resultText = playRound('rock', computerSelection)
+    if (!gameDone) {
+        const computerSelection = getComputerChoice();
+        const resultText = playRound('rock', computerSelection)
 
-    // insert hands
-    playerHand.textContent = 'Rock';
-    computerHand.textContent = computerSelection;
-    roundResult.textContent = resultText;
+        // insert hands
+        playerHand.textContent = 'Rock';
+        computerHand.textContent = computerSelection;
+        roundResult.textContent = resultText;
 
-    // update score and see if there is a winner
-    updateScore(resultText);
+        // update score and see if there is a winner
+        updateScore(resultText);
+    }
 })
 
 // paper
 const paperSection = document.querySelector(".paper")
 paperSection.addEventListener("click", function () {
-    const computerSelection = getComputerChoice();
-    const resultText = playRound('paper', computerSelection)
+    if (!gameDone){
+        const computerSelection = getComputerChoice();
+        const resultText = playRound('paper', computerSelection)
 
-    // insert hands
-    playerHand.textContent = 'Paper';
-    computerHand.textContent = computerSelection;
-    roundResult.textContent = resultText;
+        // insert hands
+        playerHand.textContent = 'Paper';
+        computerHand.textContent = computerSelection;
+        roundResult.textContent = resultText;
 
-    // update score and see if there is a winner
-    updateScore(resultText);
+        // update score and see if there is a winner
+        updateScore(resultText);
+    }
 })
 
 // scissors
 const scissorsSection = document.querySelector(".scissors")
 scissorsSection.addEventListener("click", function () {
-    const computerSelection = getComputerChoice();
-    const resultText = playRound('scissors', computerSelection)
+    if (!gameDone) {
+        const computerSelection = getComputerChoice();
+        const resultText = playRound('scissors', computerSelection)
 
-    // insert hands
-    playerHand.textContent = 'Scissors';
-    computerHand.textContent = computerSelection;
-    roundResult.textContent = resultText;
+        // insert hands
+        playerHand.textContent = 'Scissors';
+        computerHand.textContent = computerSelection;
+        roundResult.textContent = resultText;
 
-    // update score and see if there is a winner
-    updateScore(resultText);
+        // update score and see if there is a winner
+        updateScore(resultText);
+    }
 })
 
 
